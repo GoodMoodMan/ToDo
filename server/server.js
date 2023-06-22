572ece4afb3f037c5fa0c007f092c9acada7e218
unknown <goodmoodman@gmail.com>
2023-06-22 17:47:44 +0300

    fixed

--- server/server.js
+++ server/server.js
@@ -18,42 +18,48 @@ mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch(error => {
-  console.error('Error connecting to MongoDB:', error);
-});
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 // login verification
 app.post('/users/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log(username);
-    User.findOne({ username })
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({ error: 'User not found' });
-        }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const { username, password } = req.body;
+  console.log(username);
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
 
-        if (password !== user.password) {
-            return res.status(401).json({ error: 'Invalid password' });
-          }
-
-
-        res.json(user);
-      })
-      .catch(error => {
-        console.error('Error fetching user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      });
-  });
+      if (password !== user.password) {
+        return res.status(401).json({ error: 'Invalid password' });
+      }
 
 
-  // signup user post
-  app.post('/users/signup', (req,res) => {
-    const { username, password } = req.body;
-    console.log(username);
-    User.findOne({ username })
+      res.json(user);
+    })
+    .catch(error => {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+
+// signup user post
+app.post('/users/signup', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const { username, password } = req.body;
+  console.log(username);
+  User.findOne({ username })
     .then(existingUser => {
       if (existingUser) {
         // Username already taken
@@ -65,8 +71,8 @@ app.post('/users/login', (req, res) => {
           username: username,
           password: password,
           tasks: [
-            { id: 'Task 1', content: 'Description 1', date: new Date().toISOString()},
-            { id: 'Task 2', content: 'Description 2' , date: new Date().toISOString()},
+            { id: 'Task 1', content: 'Description 1', date: new Date().toISOString() },
+            { id: 'Task 2', content: 'Description 2', date: new Date().toISOString() },
             // Add more tasks as needed
           ]
         });
@@ -74,45 +80,51 @@ app.post('/users/login', (req, res) => {
         return res.status(200).send('Signup Successful');
       }
     })
-   
+
     .catch(error => {
       console.error('Error signing up user:', error);
       return res.status(500).json({ error: 'Internal server error' });
     });
-  });
+});
 
-  // task list update
-  // 
-  app.put('/users/:username/tasks', (req, res) => {
-    const { username } = req.params;
-    const { tasks } = req.body;
-    console.log("START");
-    User.findOneAndUpdate(
-        { username: username }, // criteria to find the user
-        { $set: { tasks: tasks } }, // Update operation
-        { new: true } // Options: Return the updated document
-      )
-        .then(updatedUser => {
-          if (updatedUser) {
-            // User found and updated successfully
-            console.log('User tasks updated:', updatedUser);
-            console.log("Finish Update");
-            res.status(200).send('Task list updated successfully');
-          } else {
-            // User not found
-            console.log('User not found');
-            res.status(404).json({ error: 'User not found' });
-          }
-        })
-        .catch(error => {
-          // Error occurred
-          console.log('Error updating user tasks:', error);
-          res.status(500).json({ error: 'Internal server error' });
-        });
-    
-  });
+// task list update
+// 
+app.put('/users/:username/tasks', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const { username } = req.params;
+  const { tasks } = req.body;
+  console.log("START");
+  User.findOneAndUpdate(
+    { username: username }, // criteria to find the user
+    { $set: { tasks: tasks } }, // Update operation
+    { new: true } // Options: Return the updated document
+  )
+    .then(updatedUser => {
+      if (updatedUser) {
+        // User found and updated successfully
+        console.log('User tasks updated:', updatedUser);
+        console.log("Finish Update");
+        res.status(200).send('Task list updated successfully');
+      } else {
+        // User not found
+        console.log('User not found');
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(error => {
+      // Error occurred
+      console.log('Error updating user tasks:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+
+});
 // GET all users for admin
 app.get('/users/admin', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   User.find()
     .then(users => {
       res.json(users);
