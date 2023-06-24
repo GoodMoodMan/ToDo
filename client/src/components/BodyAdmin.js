2c0c92fc47eb524441ec9552dc23fb329253c67b
unknown <goodmoodman@gmail.com>
2023-06-24 03:24:53 +0300

    fixed

--- client/src/components/BodyAdmin.js
+++ client/src/components/BodyAdmin.js
@@ -5,8 +5,9 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 function BodyAdmin(props) {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-  // useEffect to fetch users list on initial render
+  // Fetch users list on initial render
   useEffect(() => {
     fetch(`https://${props.server_ip}/users/admin`)
       .then(response => response.json())
@@ -16,143 +17,155 @@ function BodyAdmin(props) {
       .catch(error => {
         console.error('Error fetching users:', error);
       });
-  });
+  }, [users]);
 
-  // useEffect to send PUT request when users state changes
   useEffect(() => {
-    fetch(`https://${props.server_ip}/users/admin`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(users),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('User list updated:', data);
+    if (selectedUser && selectedUser.tasks) {
+      fetch(`https://${props.server_ip}/users/${selectedUser.username}/tasks`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ tasks: selectedUser.tasks }),
       })
-      .catch(error => {
-        console.error('Error updating user list:', error);
-      });
-  }, [users]); // Dependency array with users state
-
-  
-  const handleTaskContentChange = (event, userId, taskId) => {
-    const updatedUsers = users.map(user => {
-      if (user._id === userId) {
-        const updatedTasks = user.tasks.map(task => {
-          if (task.id === taskId) {
-            return { ...task, content: event.target.value };
+        .then(response => {
+          if (response.ok) {
+            console.log('Task list updated successfully');
+          } else {
+            console.error('Failed to update task list:', response.statusText);
           }
-          return task;
+        })
+        .catch(error => {
+          console.error('Error occurred:', error);
         });
-        return { ...user, tasks: updatedTasks };
-      }
-      return user;
-    });
-    setUsers(updatedUsers);
+      console.log('Finish update');
+    }
+    
+  }, [selectedUser]);
+
+  const handleUserSelect = (userId) => {
+    const selectedUser = users.find(user => user._id === userId);
+    setSelectedUser(selectedUser);
   };
 
-  const handleTaskDateChange = (date, userId, taskId) => {
-    const updatedUsers = users.map(user => {
-      if (user._id === userId) {
-        const updatedTasks = user.tasks.map(task => {
-          if (task.id === taskId) {
-            return { ...task, date };
-          }
-          return task;
-        });
-        return { ...user, tasks: updatedTasks };
+  const handleTaskContentChange = (event, taskId) => {
+    const updatedTasks = selectedUser.tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, content: event.target.value };
       }
-      return user;
+      return task;
     });
-    setUsers(updatedUsers);
+    const updatedUser = { ...selectedUser, tasks: updatedTasks };
+    setSelectedUser(updatedUser);
   };
 
-  const handleAddTask = (userId) => {
-    const updatedUsers = users.map(user => {
-      if (user._id === userId) {
-        const newTaskId = user.tasks.length + 1;
-        const newTaskObj = {
-          id: newTaskId,
-          content: 'New Task',
-          date: new Date(),
-        };
-        const updatedTasks = [...user.tasks, newTaskObj];
-        return { ...user, tasks: updatedTasks };
+  const handleTaskDateChange = (date, taskId) => {
+    const updatedTasks = selectedUser.tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, date };
       }
-      return user;
+      return task;
     });
-    setUsers(updatedUsers);
+    const updatedUser = { ...selectedUser, tasks: updatedTasks };
+    setSelectedUser(updatedUser);
   };
 
-  const handleDeleteTask = (userId, taskId) => {
-    const updatedUsers = users.map(user => {
-      if (user._id === userId) {
-        const updatedTasks = user.tasks.filter(task => task.id !== taskId);
-        return { ...user, tasks: updatedTasks };
-      }
-      return user;
-    });
-    setUsers(updatedUsers);
+  const handleAddTask = () => {
+    const newTaskId = selectedUser.tasks.length + 1;
+    const newTaskObj = {
+      id: newTaskId,
+      content: 'New Task',
+      date: new Date(),
+    };
+    const updatedTasks = [...selectedUser.tasks, newTaskObj];
+    const updatedUser = { ...selectedUser, tasks: updatedTasks };
+    setSelectedUser(updatedUser);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = selectedUser.tasks.filter(task => task.id !== taskId);
+    const updatedUser = { ...selectedUser, tasks: updatedTasks };
+    setSelectedUser(updatedUser);
   };
 
   return (
     <div className="container">
-      <h3>All Users</h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Tasks</th>
-          </tr>
-        </thead>
-        <tbody>
+      <h3 className="text-center mt-3">All Users</h3>
+      <div className="dropdown mt-3">
+        <button
+          className="btn btn-primary dropdown-toggle "
+          type="button"
+          id="userDropdown"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Select User
+        </button>
+        <div className="dropdown-menu " aria-labelledby="userDropdown">
           {users.map(user => (
-            <tr key={user._id}>
-              <td>{user.username}</td>
-              <td>
-                {user.tasks.map((task, index) => (
-                  <div key={task.id} className="task-container d-flex justify-content-center align-items-center mb-3">
-                    <div className="task-field">
-                      <label>Task:</label>
+            <button
+              key={user._id}
+              className="dropdown-item"
+              onClick={() => handleUserSelect(user._id)}
+            >
+              {user.username}
+            </button>
+          ))}
+        </div>
+      </div>
+      {selectedUser && (
+        <div className="mt-3">
+          <h4 className="text-center">User: {selectedUser.username}</h4>
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Task</th>
+                  <th>Date</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedUser.tasks.map(task => (
+                  <tr key={task.id}>
+                    <td>
                       <input
-                        className='table-fields'
                         type="text"
                         value={task.content}
-                        onChange={event => handleTaskContentChange(event, user._id, task.id)}
+                        onChange={event => handleTaskContentChange(event, task.id)}
+                        className="form-control"
                       />
-                    </div>
-                    <div className="task-field">
-                      <label>Date:</label>
+                    </td>
+                    <td>
                       <DatePicker
                         selected={new Date(task.date)}
-                        onChange={date => handleTaskDateChange(date, user._id, task.id)}
-                        className="datepicker"
+                        onChange={date => handleTaskDateChange(date, task.id)}
                         dateFormat="dd/MM/yyyy"
+                        className="form-control"
                       />
-                    </div>
-                    <div className="task-field">
+                    </td>
+                    <td>
                       <button
                         className="btn btn-danger btn-sm"
-                        onClick={() => handleDeleteTask(user._id, task.id)}
+                        onClick={() => handleDeleteTask(task.id)}
                       >
                         Delete
                       </button>
-                    </div>
-                  </div>
+                    </td>
+                  </tr>
                 ))}
-                <button
-                  className="btn btn-primary btn-sm"
-                  onClick={() => handleAddTask(user._id)}
-                >
-                  Add Task
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </tbody>
+            </table>
+          </div>
+          <button
+            className="btn btn-primary btn-sm "
+            onClick={handleAddTask}
+          >
+            Add Task
+          </button>
+        </div>
+      )}
     </div>
   );
 }
